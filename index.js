3024262b40f631c9405319e2c0611b32abc86d17
Scott Galen <scottcharlesgalen@yahoo.com>
2024-11-14 09:36:10 -0800

    made updates to comments for html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Instructions Fork and clone the starter repositoryLinks to an external site.. Open the project in VS Code. Live serve index.html. You should be greeted with a prompt.Complete the functions described in index.js
+
 // Prompt the user for a list of integers separated by commas.
 const userInputString = prompt(
   "Please enter some integers separated by commas.",
